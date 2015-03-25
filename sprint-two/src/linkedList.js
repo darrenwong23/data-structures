a18f52cc7123b03a704cea48a6e5b0a7fa93324a
darrenwong23 <darrenwong23@gmail.com>
2015-03-25 16:33:57 -0700

    Completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var oldTail = this.tail;
+    var newTail = Node(value);
+    this.tail = newTail;
+    if (this.head === null) {
+      this.head = newTail;
+    } else {
+      // only do this is "list" is non-empty
+      oldTail.next = newTail;
+    }
   };
 
   list.removeHead = function(){
+    if(this.head === null) {
+      // if empty, do nothing
+    } else if (this.tail === this.head) {
+      //if 1 element
+      this.tail = null;
+      var result = this.head.value;
+      this.head = null;
+      return result;
+    } else {
+      //if more than 1 element
+      this.head = this.head.next;
+      return this.head.value;
+    }
   };
 
   list.contains = function(target){
+    var currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
