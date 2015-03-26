7ef52d56814620f0c02653c2c160f842b25c7d2c
darrenwong23 <darrenwong23@gmail.com>
2015-03-25 21:14:21 -0700

    Finished set and hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,19 +1,69 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  // this._storage.each(function(item, i , collection) {
+  //    = [null, null];
+  // });
+
 };
 
 HashTable.prototype.insert = function(k, v){
+  //returns index between - and this._limit by hasing key
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  //if not empty, then we have to check whether exists, it it doesn't push
+  var container = this._storage.get(i);
+  var isKeyFound = false;
+
+
+  //if EMPTY container or UNDEFINED container, do this
+  if(container === undefined ) {
+    this._storage.set(i, [[k, v]]);
+  } else {
+
+    //see whether tuple already exists for given key
+    _.each(container, function(tuple) {
+
+      if(tuple[0] === k) {
+        tuple[1] = v;
+      }
+    });
+
+    //add tuple to container if it doesn't exist
+    if(!isKeyFound || container.length ===0) container.push([k,v]);
+  }
+
+
 };
+
+
+HashTable.prototype.print = function(){
+  this._storage.print();
+}
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var result = null;
 
+  var container = this._storage.get(i);
+  if(container === undefined) return null;
+
+  //see whether container has desired tuple
+  _.each(container, function(tuple){
+    if( result === null && tuple[0] === k) result = tuple[1];
+  });
+
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var container = this._storage.get(i);
+  _.each(container,function(tuple, i, collection) {
+    if (tuple[0] === k) {
+      collection.splice(i, 1);
+    }
+  });
 };
 
 
--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -27,11 +27,14 @@ var LimitedArray = function(limit){
     for(var i = 0; i < storage.length; i++){
       callback(storage[i], i, storage);
     }
-  };
 
+  };
+  limitedArray.print = function() {
+    console.log(storage);
+  };
   var checkLimit = function(index){
-    if(typeof index !== 'number'){ throw new Error('setter requires a numeric index for its first argument'); }
-    if(limit <= index){ throw new Error('Error trying to access an over-the-limit index'); }
+    if (typeof index !== 'number') { throw new Error('setter requires a numeric index for its first argument'); }
+    if (limit <= index) { throw new Error('Error trying to access an over-the-limit index'); }
   };
 
   return limitedArray;
@@ -40,7 +43,7 @@ var LimitedArray = function(limit){
 // This is a "hashing function". You don't need to worry about it, just use it
 // to turn any string into an integer that is well-distributed between the
 // numbers 0 and `max`
-var getIndexBelowMaxForKey = function(str, max){
+var getIndexBelowMaxForKey = function(str, max) {
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
     hash = (hash<<5) + hash + str.charCodeAt(i);
