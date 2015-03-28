a698bb02e2776354964b1b0f9b83a2d7d142f293
darrenwong23 <darrenwong23@gmail.com>
2015-03-27 21:04:09 -0700

    left right rotation for redblack tree completed and working

--- sprint-two/spec/redBlackTreeSpec.js
+++ sprint-two/spec/redBlackTreeSpec.js
@@ -2,39 +2,107 @@ describe('redBlackTree', function() {
   var redBlackTree;
 
   beforeEach(function() {
-    redBlackTree = new rbTree(5);
+    redBlackTree = new rbTree(10);
   });
 
-  it('should have methods named "insert", "contains", and "remove', function() {
-    expect(redBlackTree.add).to.be.a("function");
-    expect(redBlackTree.remove).to.be.a("function");
-    expect(redBlackTree.contains).to.be.a("function");
+
+  it('intialize root', function(){
+    expect(redBlackTree.root.value).to.equal(10);
   });
 
-  it('should insert values at the correct location in the tree', function(){
-    redBlackTree.add(2);
-    redBlackTree.add(3);
-    redBlackTree.add(7);
-    redBlackTree.add(6);
-    expect(redBlackTree.left.right.value).to.equal(3);
-    expect(redBlackTree.right.left.value).to.equal(6);
-  });
+  // it('add 1 node', function(){
+  //   redBlackTree.add(2);
 
-  it('should have a working "contains" method', function(){
-    redBlackTree.add(2);
-    redBlackTree.add(3);
-    redBlackTree.add(7);
-    expect(redBlackTree.contains(7)).to.equal(true);
-    expect(redBlackTree.contains(8)).to.equal(false);
-  });
+  //   expect(redBlackTree.root.left.value).to.equal(2);
+  // });
 
-  it('should execute a callback on every value in a tree using "depthFirstLog"', function(){
+  // it('should insert values at the correct location in the tree', function(){
+  //   redBlackTree.add(2);
+  //   redBlackTree.add(3);
+  //   redBlackTree.add(7);
+  //   redBlackTree.add(6);
+  //   expect(redBlackTree.root.left.right.value).to.equal(3);
+  //   expect(redBlackTree.root.right.left.value).to.equal(6);
+  // });
+
+  // it('should have a working "contains" method', function(){
+  //   redBlackTree.add(2);
+  //   redBlackTree.add(3);
+  //   redBlackTree.add(7);
+  //   expect(redBlackTree.contains(7)).to.equal(true);
+  //   expect(redBlackTree.contains(8)).to.equal(false);
+  // });
+
+  // it('should execute a callback on every value in a tree using "bfs"', function(){
+  //   var array = [];
+  //   var func = function(value){ array.push(value); };
+  //   redBlackTree.add(2);
+  //   redBlackTree.add(3);
+  //   redBlackTree.bfs(func);
+
+  //   expect(array).to.eql([5,2,3]);
+  // });
+
+
+  // it('2ad', function(){
+  //   var array = [];
+  //   var arr2 = [];
+  //       redBlackTree.root.color = 'black';
+
+  //   var func = function(value){ array.push(value); };
+  //   var f2 = function(color){ arr2.push(color); };
+
+  //   redBlackTree.add(2);
+  //   redBlackTree.root.left.color = 'red';
+  //   redBlackTree.add(6);
+  //   redBlackTree.root.right.color = 'red';
+
+
+
+
+
+  //   redBlackTree.add(1);
+
+
+  //   redBlackTree.bfs(func);
+  //   redBlackTree.bfs2(f2);
+
+
+  //   expect(array).to.eql([5,2,1,6]);
+  //  expect(arr2).to.eql([0,1,0,1]);
+
+
+
+  // });
+
+
+    it('left', function(){
     var array = [];
-    var func = function(value){ array.push(value); };
-    redBlackTree.add(2);
-    redBlackTree.add(3);
-    redBlackTree.depthFirstLog(func);
 
-    expect(array).to.eql([5,2,3]);
+    redBlackTree.root.color = 'black';
+    var func = function(value){ array.push(value); };
+
+
+    redBlackTree.add(8);
+    redBlackTree.add(9);
+    redBlackTree.add(4);
+    redBlackTree.add(12);
+
+    redBlackTree.rightRotate(redBlackTree.root,redBlackTree.root );
+
+    redBlackTree.bfs(func);
+array = [];
+    redBlackTree.leftRotate(redBlackTree.root,redBlackTree.root );
+
+    redBlackTree.bfs(func);
+    expect(array).to.eql([5,2,1,6]);
+
+
+
+
   });
+
 });
+
+
+
--- sprint-two/src/redBlackTree.js
+++ sprint-two/src/redBlackTree.js
@@ -13,65 +13,198 @@ rbTree.prototype.rightRotate = function() {
 
 };
 
-rbTree.prototype.add = function(value, node) {
-  value = value || this.root;
+rbTree.prototype.insert = function(value, node) {
+  node = node || this.root;
 
   if (value === node.value) {
     // do nothing because node value already exists
   } else if (value < node.value) {
     if (node.left !== null) {
 
-      rbTree.prototype.add(value, node.left);
+      return rbTree.prototype.insert(value, node.left);
     } else {
-      node.left = new rbTree(value);
+      node.left = new Tnode(value, 'red');
+      node.left.parent = node;
+      return node.left;
     }
   } else { // value > node.value
     if (node.right !== null) {
-      rbTree.prototype.add(value, node.right);
+      return rbTree.prototype.insert(value, node.right);
 
     } else {
-      node.right = new rbTree(value);
+      node.right = new Tnode(value, 'red');
+      node.right.parent = node;
+      return node.right;
     }
   }
+
 };
+
+rbTree.prototype.add = function(value) {
+
+  //1) perform std BST insertioins and made the color of newly inserted as red
+  var node = rbTree.prototype.insert(value, this.root);
+
+  //2)fix Insert
+
+  //rbTree.prototype.fixInsert(this.root, node);
+
+}
+
+
+rbTree.prototype.fixInsert = function(root, node) {
+
+  //if(node.value ===1) debugger;
+
+  //iterate until node is not the root and z's parent color is red
+  while ( node !== root && node.parent.color === 'red') {
+
+    var uncle;
+    //find uncle and store uncle in y
+    if (node.parent === node.parent.parent.left) {
+      uncle = node.parent.parent.right;
+    } else {
+      uncle = node.parent.parent.left;
+    }
+
+    // if uncle is RED, do following
+    //i) change color of parent and uncle as BLACK
+    //ii) change color of gp as RED
+    //iii) move z to grand parent
+    if (uncle.color === 'red') {
+      node.parent.color = 'black';
+      uncle.color = 'black';
+
+      node.parent.parent.color = 'red';
+
+      node = node.parent.parent;
+    }
+
+    else {
+
+
+
+    }
+
+
+  }// end of while loop
+
+  this.root.color
+
+};
+
+rbTree.prototype.leftRotate = function(root, x) {
+
+  //y points to right child of x
+  var y = x.right;
+
+  // store y's left subtree to x's right child
+  x.right = y.left;
+
+  //update parent point of x's right
+  if(x.right != null) {
+    x.right.parent = x;
+  }
+
+  //update y's parent pointer
+  y.parent = x.parent;
+
+  //if x's parent is null make y root of the rbTree
+  if(x.parent === null) {
+    this.root = y;
+  }
+  //replace y with x
+  else if (x === x.parent.left) {
+    x.parent.left = y;
+  } else {
+    x.parent.right = y;
+  }
+
+  //make x as left child of y
+  y.left = x;
+
+  x.parent = y;
+
+}
+
+
+rbTree.prototype.rightRotate = function(root, y) {
+
+  var x = y.left;
+
+  y.left = x.right;
+
+  if(x.right != null) {
+    x.right.parent = y;
+  }
+
+  x.parent = y.parent;
+
+  if(x.parent === null) {
+    this.root = x;
+  }
+  else if (y === y.parent.left) {
+    y.parent.left = x;
+  } else {
+    y.parent.right = x;
+  }
+
+  x.right = y;
+
+  y.parent = x;
+}
+
 
 rbTree.prototype.remove = function() {
 
 };
 
 
-
-
-
-rbTree.prototype.contains = function(node, value) {
+rbTree.prototype.contains = function(value, node) {
+    node = node || this.root;
     if (value === node.value) {
     return true;
   } else if (value < node.value) {
-    return node.left === null ? false : node.left.contains(value);
+    return node.left === null ? false : rbTree.prototype.contains(value, node.right);
   } else {
-    return node.right === null ? false : node.right.contains(value);
+    return node.right === null ? false : rbTree.prototype.contains(value, node.right);
   }
 
 };
 
 
-rbTree.prototype.bfs = function(node, callback) {
+rbTree.prototype.bfs = function(callback, node) {
+
+  node = node || this.root;
   callback(node.value);
   if (node.left !== null) {
-    node.left.bfs(node, callback);
+    rbTree.prototype.bfs(callback, node.left);
   }
   if (node.right !== null) {
-    node.right.bfs(node, callback);
+    rbTree.prototype.bfs(callback, node.right);
   }
 };
 
+rbTree.prototype.bfs2 = function(callback, node) {
+
+  node = node || this.root;
+  callback(node.color);
+  if (node.left !== null) {
+    rbTree.prototype.bfs2(callback, node.left);
+  }
+  if (node.right !== null) {
+    rbTree.prototype.bfs2(callback, node.right);
+  }
+  //console.dir(node);
+};
 
 var Tnode = function(value, color) {
 
   this.value = value || null;
 
   //black is 1, red is 0
-  this.color = color || 1;
+  this.color = color || 'red';
+
 
   this.left = null;
   this.right = null;
